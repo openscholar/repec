0728eac90a018338660bdee3b6e8613aefbbd218
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-04 18:18:53 +0530

    other template support - init

---
+++ src/Series/ecbook/Template.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\repec\Series\ecbook;
+
+use Drupal\repec\Series\Base;
+
+/**
+ * Template class for ecbook.
+ */
+class Template extends Base {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefault(): array {
+    /** @var array $default */
+    $default = parent::getDefault();
+    $default[] = [
+      'attribute' => 'Template-Type',
+      'value' => 'ReDIF-Book 1.0',
+    ];
+
+    return $default;
+  }
+
+}
