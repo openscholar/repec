2751288f629b2b3a9a373e8414a527d64ef04976
Subhojit Paul <subhojitpaul21@gmail.com>
2019-03-14 14:47:33 +0530

    book chapter template

--- src/Series/ecchap/Template.php
+++ src/Series/ecchap/Template.php
@@ -1,9 +1,26 @@
 <?php
 
-
 namespace Drupal\repec\Series\ecchap;
 
+use Drupal\repec\Series\Base;
 
-class Template {
+/**
+ * Template for book chapter.
+ */
+class Template extends Base {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefault(): array {
+    /** @var array $default */
+    $default = parent::getDefault();
+    $default[] = [
+      'attribute' => 'Template-Type',
+      'value' => 'ReDIF-Chapter 1.0',
+    ];
+
+    return $default;
+  }
 
 }
--- src/Series/eccode/Template.php
+++ src/Series/eccode/Template.php
@@ -1,9 +1,26 @@
 <?php
 
-
 namespace Drupal\repec\Series\eccode;
 
+use Drupal\repec\Series\Base;
 
-class Template {
+/**
+ * Template software component article.
+ */
+class Template extends Base {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefault(): array {
+    /** @var array $default */
+    $default = parent::getDefault();
+    $default[] = [
+      'attribute' => 'Template-Type',
+      'value' => 'ReDIF-Software 1.0',
+    ];
+
+    return $default;
+  }
 
 }
